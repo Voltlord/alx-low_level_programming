9712c96bc9aabaa9d0c9bf12e827eb23a2292eaf
Voltlord <narh953@gmail.com>
2022-10-18 12:17:25 -0700

    0x02-functions_nested_loops task#0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* int main(void) */
+/*
+ *Printing _putchar
+ *
+ *main is the entry point
+ *
+ *Return zero if successful
+*/
+{
+	printf("_putchar\n");
+
+	return (0);
+}
