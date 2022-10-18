e0dd699478ea53902a4e01655d21a077b96d192c
Voltlord <narh953@gmail.com>
2022-10-17 21:55:34 -0700

    .

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+*To produce combinations of 2 digits
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
-{
-	int num;
+{                                                         int num1, num2;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
+        for (num1 = 0; num1 <= 98; num1++)
+        {
+                for (num2 = num1 + 1; num2 <= 99; num2++)
+                {
+                        putchar((num1 / 10) + '0');
+                        putchar((num1 % 10) + '0');
+                        putchar(' ');
+                        putchar((num2 / 10) + '0');
+                        putchar((num2 % 10) + '0');
+		}
 	}
-
-	putchar('\n');
-
-	return (0);
 }
